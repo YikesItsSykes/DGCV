28246c7a10fecff48eccb80c59aec516babcf804
YikesItsSykes <dgsykestriad@gmail.com>
2024-10-14 20:01:56 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ To get started, check out the Jupyter Notebook tutorials:
 
 setup(
     name="DGCV",
-    version="0.1.4",
+    version="0.1.4a0",
     description="Differential Geometry with Complex Variables",
     long_description=long_description,  # This shows up on PyPI
     long_description_content_type='text/markdown',
